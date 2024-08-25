09e2521489ec7615dafea9f40312d8952483c4df
Thivakar <147425494+THIVAKAR-G@users.noreply.github.com>
2024-08-25 22:00:46 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,7 +12,7 @@ const About = () => {
           <h1 className='AboutTextHeading' >Get to <b>know</b> me!</h1>
           <p>
             Hi, my name is <b>Thivakar</b> and I am from Tamil Nadu, India.
-            I'm a <b>web developer</b> and a Second year college student pursuing <b>BE in CSE</b>. <br/><br/>
+            I'm a <b>web developer</b> and a Pre-final year college student pursuing <b>BE in CSE</b>. <br/><br/>
             I have done an internship as a <b>web developer </b> at Octanet which is a fintech startup.
             I love to create original projects with beautiful designs, you can check out some of my work in the projects section.<br/><br/>
             I am <b>open</b> to new collaborations or work where I can contribute and grow. Feel free to connect with me, links are in the footer.<br/>
@@ -41,6 +41,9 @@ const About = () => {
         <Skills skill='Vercel' />
         <Skills skill='Npm' />
         <Skills skill='Bootstrap'/>
+        <Skills skill='Express' />
+        <Skills skill='MongoDB' />
+        <Skills skill='Express'/>
       </div>
     </>
   )
