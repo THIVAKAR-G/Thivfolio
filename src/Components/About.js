8d7423f6b0150aaa6f55a1146631da4f10bada4d
Thivakar <147425494+THIVAKAR-G@users.noreply.github.com>
2024-08-25 22:04:59 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -43,7 +43,6 @@ const About = () => {
         <Skills skill='Bootstrap'/>
         <Skills skill='Express' />
         <Skills skill='MongoDB' />
-        <Skills skill='Express'/>
       </div>
     </>
   )
