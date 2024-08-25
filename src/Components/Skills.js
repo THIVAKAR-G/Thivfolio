760c16cd8d60a7ffc66fffc562733e6ab0902f56
Thivakar <147425494+THIVAKAR-G@users.noreply.github.com>
2024-08-25 22:33:57 +0530

    Update Skills.js

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import { CgCPlusPlus } from "react-icons/cg";
-import { FaReact, FaPython, FaGitAlt, FaGithub, FaNpm, FaFigma, FaBootstrap } from "react-icons/fa";
+import { FaReact, FaPython, FaGitAlt, FaGithub, FaNpm, FaFigma, FaBootstrap, FaJava} from "react-icons/fa";
 import { DiNodejs, DiJavascript1 } from "react-icons/di";
 import { SiExpress, SiMongodb, SiPostman, SiVercel } from "react-icons/si";
 
@@ -18,6 +18,7 @@ const Skills = ({ skill }) => {
         // Figma: <FaFigma/>,
         Bootstrap: <FaBootstrap />,
         Vercel: <SiVercel />
+        Java: <FaJava />
     }
     
     return (
