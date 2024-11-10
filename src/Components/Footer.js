d84bba37c89c7142d6da462b4efda2feab05bcf2
Thivakar <147425494+THIVAKAR-G@users.noreply.github.com>
2024-11-10 14:56:57 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
       <h4>Developed by Thivakar</h4>
       <h4>Copyright &copy; 2024 </h4>
       <div className='footerLinks'>
-        <a href="https://github.com/THIVAKAR" target='_blank'><FaGithub/></a>
+        <a href="https://github.com/THIVAKAR-G" target='_blank'><FaGithub/></a>
         <a href="https://www.linkedin.com/in/thivakar-g-481135288?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='_blank'><FaLinkedin/></a>
         <a href='mailTo:thivakardixit@gmail.com' target='_blank'><GrMail/></a>
         {/* <a href="https://leetcode.com/sahni1403/" target="_blank"><SiLeetcode/></a> */}
